7b1b6d9c265783bc40b52db316d00f56678cc631
John Yu <john.yu@reisystems.com>
2018-11-08 22:45:38 -0500

    socket-io.js updates/tests

--- src/util/socket-io.js
+++ src/util/socket-io.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
-const socket = io("localhost:3000");
+const socket = io("http://107.23.242.171");
+// const socket = io("localhost:4040");
 
 // socket.emit("message", { message: "hello world" });
 module.exports = socket;
